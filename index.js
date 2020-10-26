9185302d34fc49f70da76e267e6a4a489d2ddf0e
brynknowles <bryn.knowles@gmail.com>
2020-10-26 13:52:23 -0400

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(namesArr, event) {
+    let cards = []
+    for (let i = 0; i < namesArr.length; i++) {
+        cards.push(`Thank you, ${namesArr[i]}, for the wonderful ${event} gift!`)
+    }
+    return cards
+}
+
+function countDown(num) {
+    while (num > 0) {
+        console.log(num)
+        num -= 1
+    }
+    console.log(num)
+}
